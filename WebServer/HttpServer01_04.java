64b32055233ec71e945c4ab7d9cd27d2fcc8055a
Bernardo Meyer <55299005+b3rn4rd0m3y3r@users.noreply.github.com>
2024-06-25 22:19:38 -0300

    Add files via upload

--- WebServer/HttpServer01_04.java
+++ WebServer/HttpServer01_04.java
@@ -1,25 +1,27 @@
 import java.net.ServerSocket;
 import java.nio.charset.*;
 import java.io.*;
-//import java.util.Map;
-//import java.util.List;
 import java.util.*;
 import java.lang.*;
-//import java.lang.Record;
 import java.net.*;
 
 public class HttpServer01_04{  
-    public static void main(String[] args) throws Exception{  
-		var serverSocket = new ServerSocket(8098); 
+    private static final Charset ASCII = StandardCharsets.ASCII;
+	private static final int PORT = 8098;
+	public static void main(String[] args) throws Exception{  
+		
+		var serverSocket = new ServerSocket(PORT);
 		var header = "HTTP/1.1 200 OK\nContent-type: text/html\nContent-length: %d\n\n%s";
+		System.out.println("\033[32mListen on port: "+PORT+"\033[0m");
 		while(true){
 			var connection = serverSocket.accept();
+			System.out.println("Cliente: "+connection);
 			var request = readRequest(connection);
-			System.out.println("Requisição: "+request);
+			System.out.println("Requisição: \033[33m"+request+"\033[0m");
 			System.out.println("_________________________________________");
 			try(var os = connection.getOutputStream()){
 				var body = "<meta charset=\"UTF-8\"><h1>Requisição</h1><h2>Deu Certo</h2>";
-				var response = String.format(header,body.getBytes(StandardCharsets.UTF_8).length, body);
+				var response = String.format(header,body.getBytes(ASCII).length, body);
 				os.write(response.getBytes(StandardCharsets.UTF_8));
 				}
 			}
@@ -41,16 +43,17 @@ public class HttpServer01_04{
 			System.out.println("Path: "+path);
 			System.out.println("Protocolo: "+protocol);
 			}
+		System.out.println("_________________________________________");
 		// Novo nesta versão 4
 		var headers = readHeaders(r);
-		//return new HttpReq(metodo, path, Map.of(), null);
-		System.out.println("Headers: "+headers);
+		System.out.println("Headers: \033[36m"+headers+"\033[0m");
 		return new HttpReq(metodo, path, headers, null);
 		}
 	// Novo nesta versão 4	
 	private static 	Map<String, List<String>> readHeaders(BufferedReader reader) throws Exception{
 		var line = reader.readLine();
 		var headers = new HashMap<String, List<String>>();
+		System.out.println("\033[31m");
 		while( line != null && !line.isEmpty()){
 			var par = line.split(":",2);
 			var chave = par[0];
@@ -59,6 +62,8 @@ public class HttpServer01_04{
 			System.out.println("Chave: "+chave+ " - " + value);
 			line = reader.readLine();
 			}
+		System.out.println("\033[0m");
+		System.out.println("_________________________________________");
 		return headers;
 		}
 		
