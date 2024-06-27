a13f9942fc00aa3cc34605237c8eb8caec925354
Bernardo Meyer <55299005+b3rn4rd0m3y3r@users.noreply.github.com>
2024-06-27 13:57:01 -0300

    Add files via upload

---
+++ WebServer/HttpServer01_03a.java
@@ -0,0 +1,72 @@
+import java.net.ServerSocket;
+import java.nio.charset.*;
+import java.io.*;
+//import java.util.Map;
+//import java.util.List;
+import java.util.*;
+import java.lang.*;
+//import java.lang.Record;
+import java.net.*;
+
+public class HttpServer01_03a{  
+    public static void main(String[] args) throws Exception{  
+		var PORT = 8098;
+		var serverSocket = new ServerSocket(PORT); 
+		var header = "HTTP/1.1 200 OK\nContent-type: text/html;charset=utf-8\nContent-length: %d\n\n%s";
+		System.out.println("Listen on port: "+PORT);
+		while(true){
+			var connection = serverSocket.accept();
+			// Função readRequest para detalhamento
+			var request = readRequest(connection);
+			// Novo nesta versão 3
+			System.out.println("Requisição: "+request);
+			// Filename
+			var fileName = request.path;
+			fileName = (String)fileName;
+			// Filename sem barra
+			var arr = fileName.split("/");
+			fileName = arr[1];
+			System.out.println("PATH final: "+fileName);
+			arr = fileName.split("\\.");
+			System.out.println(arr[1]);
+			// Extensao
+			var extensao = arr[1].toLowerCase();
+			if( !extensao.equals("html") && !extensao.equals("htm") ){
+				System.out.println("Extensão: "+extensao+ " não será processada pelo SERVER");
+				continue;
+				}
+			try(var os = connection.getOutputStream()){
+				var body = "<h1>Deu Certo</h1>";
+				var response = String.format(header,body.getBytes(StandardCharsets.UTF_8).length, body);
+				os.write(response.getBytes(StandardCharsets.UTF_8));
+				}
+			}
+		}  
+	// Função readBody
+	
+	// Função readRequest
+	private static HttpReq readRequest(Socket connection) throws Exception{
+		var r = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+		var line = r.readLine();
+		var metodo = "";
+		var path = "";
+		var protocol = "";
+		// Novo nesta versão 3
+		// Da requisição retornam: MÉTODO PATH PROTOCOL
+		if( line != null ){	
+			var RetornoISP = line.split(" ");
+			metodo = RetornoISP[0];
+			path = RetornoISP[1];
+			protocol = RetornoISP[2];
+			/*
+			System.out.println("Metodo: "+metodo);
+			System.out.println("Path: "+path);
+			System.out.println("Protocolo: "+protocol);
+			*/
+			}
+		return new HttpReq(metodo, path, Map.of(), null);
+		}
+	private record HttpReq(String metodo, String path, Map<String, List<String>> headers, byte[] body){
+		
+		}
+	}  
